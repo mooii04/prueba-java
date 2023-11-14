d9dbaaaf9c7831376de9a65d90f520aae87e03e8
Moisés Dorado Gutiérrez <dorado.gumoi24@triana.salesianos.edu>
2023-11-14 13:55:01 +0100

    Guauuuuuuuuuu

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,8 +4,9 @@ public class HolaMundo {
 
     public static void main(String[] args){
 
+        System.out.println("*".repeat(10));
         System.out.println("Hola Mundo");
-
+        System.out.println("*".repeat(10));
 
     }
 
